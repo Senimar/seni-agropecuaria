6adf516db6abaa0722bd346a442658ecc63431f4
Marcos Seni <marcos.seni13@gmail.com>
2021-03-03 15:16:14 -0300

    versionando as alteracoes

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Seni Agropecuária</title>
+</head>
+<body>
+
+<h1>Seni Agropecuária</h1>
+<p>Especializado em Produtos Agropecuários.</p>
+
+</body>
+</html>
