e9e0dee27251d665a8e12e12a7c67665aeb72c27
Marcos Seni <marcos.seni13@gmail.com>
2021-03-03 16:35:17 -0300

    Alteração de Cores

--- index.php
+++ index.php
@@ -1,12 +1,22 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-<title>Seni Agropecuária</title>
+    <title>Seni Agropecuária</title>
 </head>
+
 <body>
 
-<h1>Seni Agropecuária</h1>
-<p>Especializado em Produtos Agropecuários.</p>
+    <h1>Seni Agropecuária</h1>
+    <p>Especializado em Produtos Agropecuários.</p>
 
 </body>
+
+</html>
+<div style="width: 100%;display:flex;height: 80px">
+    <div style="width: 50%;background: blue"></div>
+    <div style="width: 50%;background: red"></div>
+</div>
+</body>
+
 </html>
